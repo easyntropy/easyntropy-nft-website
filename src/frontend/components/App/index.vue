b5e92dd65d31408c2f514574faf02daef0cd81d6
Jakub Niewczas <niewczas.jakub@gmail.com>
2025-07-27 15:07:16 +0900

    implement header and kv

--- src/frontend/components/App/index.vue
+++ src/frontend/components/App/index.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <h1>hello world!</h1>
+  <div class="host">
+    <div class="header">
+      <a href="/" class="logo">Easyntrophy NFT</a>
+      <a href="https://easyntropy.tech/" class="poweredBy">
+        <span> Powered by Easyntropy, <br />The Eth rng Oracle </span>
+        <img src="../../assets/images/easyntropy.png" alt="Easyntropy Logo" />
+      </a>
+    </div>
+    <div class="kv">
+      <h1>hello world!</h1>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss">
+@import "./css/fonts";
 @import "./css/variables.scss";
 @import "./css/normalize";
 
 html {
-  font-size: 8px;
+  font-size: 16px;
 }
 
 html,
@@ -19,7 +29,7 @@ body,
 #app {
   height: 100%;
   color: $text;
-  font-family: Tahoma, sans-serif;
+  font-family: Minecraft-Regular, Tahoma, Geneva, sans-serif;
 }
 
 html,
@@ -53,5 +63,62 @@ h5 {
   @media screen and (max-width: $md) {
     display: none;
   }
+}
+
+// -----
+
+.host {
+  position: relative;
+  width: 100%;
+  min-height: 100%;
+  background: #97450e;
+}
+
+.header {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 60px;
+  padding: 0 20px;
+  background-color: #0000004b;
+
+  .logo {
+    color: $text;
+    color: white;
+    font-weight: bold;
+    font-size: 1.5rem;
+    text-decoration: none;
+  }
+
+  .poweredBy {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    color: $text;
+    color: #e5c9ff;
+    font-weight: bold;
+    font-size: 0.8rem;
+    text-decoration: none;
+    filter: drop-shadow(0 0 5px #a852fb);
+
+    img {
+      width: 45px;
+      height: 45px;
+    }
+  }
+}
+
+.kv {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: max(60vh, 400px);
+  background: url("../assets/images/kv.png") no-repeat 50% 100%;
+  background-size: cover;
 }
 </style>

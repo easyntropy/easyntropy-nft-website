52391ee561ae39509eee0f161dec27bfbef53fa2
Jakub Niewczas <niewczas.jakub@gmail.com>
2025-07-29 16:48:25 +0900

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.BASE_URL || "/easyntrophy/",
   build: {
     outDir: "../../dist/public",
     target: "esnext",

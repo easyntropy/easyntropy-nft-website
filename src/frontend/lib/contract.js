707178da30685c05c9d514b2bdf3d10e5e492b46
Jakub Niewczas <niewczas.jakub@gmail.com>
2025-08-25 23:19:41 +0900

    refactor read/write contract

--- src/frontend/lib/contract.js
+++ src/frontend/lib/contract.js
@@ -1,35 +1,24 @@
 import { ethers } from "ethers";
+import chains from "./chains";
+
+const contractAddresses = {
+  "0x7a69": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // dev chain
+  "0xaa36a7": "0xA7ac0896Ebbe0E70Ad6Ce97f3AB083E76a960617", // sepolia testnet
+  "0x1": "0xBc3489B963CC5E44f90E6f559144B0AAEe1B31C6", // main net
+};
+
+const contractABI = [
+  "function mint() public",
+  "function easyntropyFee() public view returns (uint256 fee)",
+  "function tokenURI(uint256 tokenId) public view returns (string memory result)",
+  "function ownedTokens(address tokensOwner) external view returns (uint256[] memory result)",
+  "function seeds(uint256 tokenId) public view returns (uint256)",
+];
 
 export async function getReadContract(wallet) {
-  let contractAddress;
-  let rpcUrl;
   const chainId = wallet.chains[0].id;
-  //
-  // dev chain
-  if (chainId === "0x7a69") {
-    contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-    rpcUrl = "http://localhost:8545";
-
-    //
-    // sepolia testnet
-  } else if (chainId === "0xaa36a7") {
-    contractAddress = "0xA7ac0896Ebbe0E70Ad6Ce97f3AB083E76a960617";
-    rpcUrl = "https://sepolia.drpc.org";
-
-    //
-    // mainnet
-  } else {
-    contractAddress = "0xBc3489B963CC5E44f90E6f559144B0AAEe1B31C6";
-    rpcUrl = "https://eth.drpc.org";
-  }
-
-  const contractABI = [
-    "function mint() public",
-    "function easyntropyFee() public view returns (uint256 fee)",
-    "function tokenURI(uint256 tokenId) public view returns (string memory result)",
-    "function ownedTokens(address tokensOwner) external view returns (uint256[] memory result)",
-    "function seeds(uint256 tokenId) public view returns (uint256)",
-  ];
+  const contractAddress = contractAddresses[chainId];
+  const rpcUrl = chains.find((chain) => chain.id === chainId)?.rpcUrl;
 
   const provider = new ethers.JsonRpcProvider(rpcUrl);
   const contract = new ethers.Contract(contractAddress, contractABI, provider);
@@ -37,31 +26,8 @@ export async function getReadContract(wallet) {
 }
 
 export async function getWriteContract(wallet) {
-  let contractAddress;
-
-  //
-  // dev chain
-  if (wallet.chains[0].id === "0x7a69") {
-    contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-
-    //
-    // sepolia testnet
-  } else if (wallet.chains[0].id === "0xaa36a7") {
-    contractAddress = "0xA7ac0896Ebbe0E70Ad6Ce97f3AB083E76a960617";
-
-    //
-    // mainnet
-  } else {
-    contractAddress = "0xBc3489B963CC5E44f90E6f559144B0AAEe1B31C6";
-  }
-
-  const contractABI = [
-    "function mint() public",
-    "function easyntropyFee() public view returns (uint256 fee)",
-    "function tokenURI(uint256 tokenId) public view returns (string memory result)",
-    "function ownedTokens(address tokensOwner) external view returns (uint256[] memory result)",
-    "function seeds(uint256 tokenId) public view returns (uint256)",
-  ];
+  const chainId = wallet.chains[0].id;
+  const contractAddress = contractAddresses[chainId];
 
   const provider = new ethers.BrowserProvider(wallet.provider);
   const signer = await provider.getSigner();
--- src/frontend/lib/wallet-connection.js
+++ src/frontend/lib/wallet-connection.js
@@ -1,33 +1,11 @@
 import { init } from "@web3-onboard/vue";
 import injectedModule from "@web3-onboard/injected-wallets";
 import logo from "../assets/images/easyntropy.png";
+import chains from "./chains";
 
 init({
   wallets: [injectedModule()],
-  chains: [
-    ...(import.meta.env.DEV
-      ? [
-          {
-            id: "0x7a69", // 31337,
-            token: "ETH",
-            label: "DEV Chain",
-            rpcUrl: "http://localhost:8545",
-          },
-        ]
-      : []),
-    {
-      id: "0x1",
-      token: "ETH",
-      label: "Ethereum Mainnet",
-      rpcUrl: "https://eth.drpc.org",
-    },
-    {
-      id: "0xaa36a7",
-      token: "ETH",
-      label: "Sepolia Testnet",
-      rpcUrl: "https://sepolia.drpc.org",
-    },
-  ],
+  chains,
   appMetadata: {
     name: "Easyntrophy",
     icon: logo,
